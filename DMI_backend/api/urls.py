add6b3fb9bc6163d7727e7143c7bd68ca620a251
Spec.psx <69973760+Spectrewolf8@users.noreply.github.com>
2024-12-17 00:19:06 +0500

    feat: update JWT settings, enhance token handling, and add refresh token endpoint

--- DMI_backend/api/urls.py
+++ DMI_backend/api/urls.py
@@ -5,9 +5,6 @@ urlpatterns = [
     path("signup/", views.signup, name="signup"),
     path("login/", views.login, name="login"),
     path("logout/", views.logout, name="logout"),
-    path(
-        "process-deepfake-media/",
-        views.process_deepfake_media,
-        name="process-deepfake-media",
-    ),
+    path("process/df/", views.process_deepfake_media, name="process-deepfake"),
+    path("refresh_token/", views.refresh_token, name="refresh_token"),
 ]
--- DMI_backend/api/views.py
+++ DMI_backend/api/views.py
@@ -1,3 +1,4 @@
+import time
 from django.views.decorators.csrf import csrf_exempt
 from django.core.files.storage import FileSystemStorage
 from django.conf import settings
@@ -5,10 +6,11 @@ from django.http import JsonResponse
 from django.contrib.auth.models import User
 
 from rest_framework.decorators import api_view, permission_classes, parser_classes
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework import status
 from rest_framework.parsers import MultiPartParser, FormParser, FileUploadParser
-from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework_simplejwt.tokens import RefreshToken, AccessToken, TokenError
+from rest_framework_simplejwt.exceptions import InvalidToken, TokenError
 
 from app.contollers.DeepfakeDetectionController import DeepfakeDetectionPipeline
 from .serializers import FileUploadSerializer, UserSerializer, LoginSerializer
@@ -34,8 +36,11 @@ pipeline = DeepfakeDetectionPipeline(
 def signup(request):
     serializer = UserSerializer(data=request.data)
     if serializer.is_valid():
-        serializer.save()
-        return JsonResponse(serializer.data, status=status.HTTP_201_CREATED)
+        try:
+            serializer.save()
+            return JsonResponse(serializer.data, status=status.HTTP_201_CREATED)
+        except Exception as e:
+            return JsonResponse({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
     return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
@@ -46,29 +51,47 @@ def login(request):
     if serializer.is_valid():
         user = serializer.validated_data
         refresh = RefreshToken.for_user(user)
+        access = refresh.access_token
         return JsonResponse(
             {
                 "refresh": str(refresh),
-                "access": str(refresh.access_token),
-            }
+                "access": str(access),
+            },
+            status=status.HTTP_200_OK,
         )
     return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 @api_view(["POST"])
+@permission_classes([AllowAny])
 def logout(request):
     try:
-        refresh_token = request.data["refresh"]
+        refresh_token = request.data.get("refresh")
+        if not refresh_token:
+            return JsonResponse(
+                {"error": "Refresh token is required."},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
         token = RefreshToken(refresh_token)
         token.blacklist()
-        return JsonResponse(status=status.HTTP_205_RESET_CONTENT)
+        return JsonResponse(
+            {"message": "Successfully logged out."},
+            status=status.HTTP_205_RESET_CONTENT,
+        )
+
+    except TokenError as e:
+        return JsonResponse(
+            {"error": "Token is invalid or expired."},
+            status=status.HTTP_401_UNAUTHORIZED,
+        )
     except Exception as e:
-        return JsonResponse(status=status.HTTP_400_BAD_REQUEST)
+        return JsonResponse({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
 
 
 @csrf_exempt
 @api_view(["POST"])
-@permission_classes([AllowAny])
+@permission_classes([IsAuthenticated])
 @parser_classes([MultiPartParser, FormParser, FileUploadParser])
 def process_deepfake_media(request):
     serializer = FileUploadSerializer(data=request.data)
@@ -77,10 +100,13 @@ def process_deepfake_media(request):
         try:
             validated_data = serializer.validated_data
             media_file = validated_data["file"]
+            user = request.user
 
             # Save file
             fs = FileSystemStorage(location=f"{settings.MEDIA_ROOT}/submissions/")
-            filename = fs.save(media_file.name, media_file)
+            filename = fs.save(
+                f"uid{user.id}-{time.time()}-{media_file.name}", media_file
+            )
             file_path = os.path.join(f"{settings.MEDIA_ROOT}/submissions/", filename)
 
             # Process media
@@ -89,26 +115,42 @@ def process_deepfake_media(request):
                 frame_rate=2,
             )
 
-            # # Add metadata
-            # results.update(
-            #     {
-            #         "title": validated_data.get("title", ""),
-            #         "description": validated_data.get("description", ""),
-            #         "file_type": validated_data.get("file_type", ""),
-            #         "file_size": validated_data.get("file_size", ""),
-            #         "upload_date": (
-            #             validated_data.get("upload_date", "").isoformat()
-            #             if validated_data.get("upload_date")
-            #             else ""
-            #         ),
-            #     }
-            # )
-
             return JsonResponse(results, status=status.HTTP_200_OK)
 
+        except TokenError as e:
+            return JsonResponse(
+                {"error": "Token is invalid or expired."},
+                status=status.HTTP_401_UNAUTHORIZED,
+            )
         except Exception as e:
             return JsonResponse(
                 {"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR
             )
     else:
         return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(["POST"])
+@permission_classes([AllowAny])
+def refresh_token(request):
+    try:
+        refresh_token = request.data.get("refresh")
+        if not refresh_token:
+            return JsonResponse(
+                {"error": "Refresh token is required."},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+        token = RefreshToken(refresh_token)
+        access_token = token.access_token
+        return JsonResponse(
+            {"access": str(access_token)},
+            status=status.HTTP_200_OK,
+        )
+    except TokenError as e:
+        return JsonResponse(
+            {"error": "Token is invalid or expired."},
+            status=status.HTTP_401_UNAUTHORIZED,
+        )
+    except Exception as e:
+        return JsonResponse({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
