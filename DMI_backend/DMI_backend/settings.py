5d4069e0ab74016e8b4908e6d30fdfbf50b177aa
Spec.psx <salmantariq8385@gmail.com>
2025-05-12 23:50:37 +0500

    Refactor admin and moderation portal templates; remove old login page and add new access denied page. Implement analytics dashboard for forum activity with enhanced styling and functionality.

--- DMI_backend/DMI_backend/settings.py
+++ DMI_backend/DMI_backend/settings.py
@@ -76,7 +76,7 @@ ROOT_URLCONF = "DMI_backend.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [os.path.join(BASE_DIR, "templates")],
+        "DIRS": [os.path.join(BASE_DIR, "templates"), os.path.join(BASE_DIR, "_templates")],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
